be4dd0cb7507f516367ae4e4d746af5924804edc
Asen <asen.trichkov1@gmail.com>
2023-11-20 01:29:10 +0200

    Initial commit

---
+++ run.rb
@@ -0,0 +1,50 @@
+require 'docx'
+
+$sourceFileName = 'input.docx';
+$destinationFileName = 'result.docx';
+puts "Please enter values for replacement with spaces (current -> new):"
+ReplaceValues = {}
+input = gets.chomp
+names = input.split
+
+names.each_with_index do |name, index|
+  next if name == 'done'
+  if index % 2 == 0
+    ReplaceValues[name.upcase.to_sym] = ""
+  else 
+    puts ReplaceValues.keys.last
+    ReplaceValues[ReplaceValues.keys.last] = name.upcase
+  end
+end
+
+puts "Check key/value paris for replacement and press enter to continue"
+puts ReplaceValues
+gets.chomp # wait for confirmation
+
+def processDocument(replaceValues)
+  # Create a Docx::Document object for our existing docx file
+  doc = Docx::Document.open($sourceFileName)
+
+  doc.tables.each do |table|
+    table.rows.each do |(row, val), idx| # Row-based iteration
+      counter = 0;
+      row.cells.each do |cell|
+        cell.paragraphs.each do |paragraph|
+          paragraph.each_text_run do |text|
+            if counter == 1 and replaceValues.include? paragraph.text.to_sym
+              text.substitute(text.to_s, replaceValues[paragraph.text.to_sym])
+            end
+          end
+        end
+        counter += 1
+      end
+      counter = 0
+    end
+  end
+
+  doc.save($destinationFileName)
+  puts "New file #{$destinationFileName} is generated"
+  puts 
+end
+
+processDocument(ReplaceValues)
