0b7422364b982461e4b6beb51390c1922382f10a
Asen <asen.trichkov1@gmail.com>
2023-11-25 01:59:48 +0200

    Refactor variable names

--- bin/replacer.rb
+++ bin/replacer.rb
@@ -5,56 +5,55 @@ require 'docx'
 DATADIR = File.join(__dir__, '../', 'data/')
 
 puts 'Enter source file name (if you press enter to skip input.docx would be used)'
-sourceFile = gets.chomp
+source_file = gets.chomp
 
-if sourceFile == ""
-  sourceFile = 'input.docx'
+if source_file == ''
+  source_file = 'input.docx'
 end
 
-$sourceFileName = "#{DATADIR}#{sourceFile}"
-puts $sourceFileName
-$destinationFileName = "#{DATADIR}result.docx"
-puts "Please enter values for replacement separated by comma (current -> new):"
-ReplaceValues = {}
+$source_file_path = "#{DATADIR}#{source_file}"
+$destination_file_path = "#{DATADIR}result.docx"
+puts 'Please enter values for replacement separated by comma (current -> new):'
+replace_values = {}
 input = gets.chomp
 names = input.split(',')
 
 names.each_with_index do |name, index|
   name = name.upcase.strip
   if index % 2 == 0
-    ReplaceValues[name.to_sym] = ""
+    replace_values[name.to_sym] = ''
   else 
-    ReplaceValues[ReplaceValues.keys.last] = name
+    replace_values[replace_values.keys.last] = name
   end
 end
 
-puts "Check current/new pairs for replacement and press enter to continue"
-puts ReplaceValues
+puts 'Check current/new pairs for replacement and press enter to continue'
+puts replace_values
 gets.chomp # wait for confirmation
 
-def processDocument(replaceValues)
+def processDocument(replace_values)
   # Create a Docx::Document object for our existing docx file
-  doc = Docx::Document.open($sourceFileName)
+  doc = Docx::Document.open($source_file_path)
 
   doc.tables.each do |table|
     table.rows.each do |(row, val), idx| # Row-based iteration
-      counter = 0;
+      cell_counter = 0;
       row.cells.each do |cell|
         cell.paragraphs.each do |paragraph|
           paragraph.each_text_run do |text|
-            if counter == 1 and replaceValues.include? paragraph.text.to_sym
-              text.substitute(text.to_s, replaceValues[paragraph.text.to_sym])
+            if cell_counter == 1 and replace_values.include? paragraph.text.to_sym
+              text.substitute(text.to_s, replace_values[paragraph.text.to_sym])
             end
           end
         end
-        counter += 1
+        cell_counter += 1
       end
-      counter = 0
+      cell_counter = 0
     end
   end
 
-  doc.save($destinationFileName)
-  puts "New file #{$destinationFileName} is generated"
+  doc.save($destination_file_path)
+  puts "New file #{$destination_file_path} is generated"
 end
 
-processDocument(ReplaceValues)
+processDocument(replace_values)
